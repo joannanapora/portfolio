66e8fefebaf2390beee3e7c6014fee24009279b6
Joanna Napora <joannanapora@Joannas-Air.home>
2022-02-07 15:14:35 +0000

    removed kocie app

--- src/projects/projects.tsx
+++ src/projects/projects.tsx
@@ -8,15 +8,14 @@ import {
   faHandPointer,
   faRunning,
   faShoppingCart,
-  faCat
+  faCat,
 } from "@fortawesome/free-solid-svg-icons";
 import { faSlack, faBitcoin } from "@fortawesome/free-brands-svg-icons";
 
 const Projects = () => {
-
   return (
     <div className="project-body">
-        <div className="card">
+      <div className="card">
         <FontAwesomeIcon
           size="9x"
           color="#fff"
@@ -33,13 +32,13 @@ const Projects = () => {
             </h1>
 
             <div className="body-text">
-              Plants Shop is a fully functional React/JavaScript app based on SASS styles.
-              During this project I got to know Firebase and how it handles authorization,
-              I learned how to operate stripe in the test version.
-              State is managed by Redux and navigation by React-Router.
+              Plants Shop is a fully functional React/JavaScript app based on
+              SASS styles. During this project I got to know Firebase and how it
+              handles authorization, I learned how to operate stripe in the test
+              version. State is managed by Redux and navigation by React-Router.
               The website is fully responsive.
               <br />
-              <br /> 
+              <br />
               <a
                 className="project-link"
                 target="_blank"
@@ -63,44 +62,6 @@ const Projects = () => {
           size="9x"
           color="#fff"
           style={{ padding: "1rem" }}
-          icon={faCat}
-        />
-        <h1 className="project-header">Browser Game</h1>
-        <div className="text1">
-          <div className="text-content">
-            <h1 className="title">
-              Web Application
-              <br />
-              Javascript/HTML/CSS/Canvas
-            </h1>
-
-            <div className="body-text">
-              This project is for desktops only. I've created KocieApp Project mainly to learn how to use Canvas.
-              Main characters are my own cats :)  <br />
-              <br /> 
-              <a
-                className="project-link"
-                target="_blank"
-                rel="noreferrer"
-                href="https://joannanapora.github.io/kocieapp/"
-              >
-                GO TO PROJECT
-              </a>
-              <FontAwesomeIcon
-                size="1x"
-                color="#fff"
-                icon={faHandPointer}
-                style={{ marginTop: "-10px" }}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-       <div className="card">
-        <FontAwesomeIcon
-          size="9x"
-          color="#fff"
-          style={{ padding: "1rem" }}
           icon={faSlack}
         />
         <h1 className="project-header">Slack Copy</h1>
@@ -114,11 +75,12 @@ const Projects = () => {
 
             <div className="body-text">
               SlackCopy is a fully functional React/JavaScript application for
-              developers to communicate.   
-              During this project I got to know how to store data and images in Firebase.
-              The design is based on Styled Components. State is managed by Redux and navigation by
-              React-Router. The website is desktops only but I'm working on its resposivity. <br />
-              <br /> 
+              developers to communicate. During this project I got to know how
+              to store data and images in Firebase. The design is based on
+              Styled Components. State is managed by Redux and navigation by
+              React-Router. The website is desktops only but I'm working on its
+              resposivity. <br />
+              <br />
               <a
                 className="project-link"
                 target="_blank"
@@ -149,8 +111,10 @@ const Projects = () => {
           <div className="text-content">
             <h1 className="title">React/Typescript</h1>
             <div className="body-text">
-            During this project I got to know more about routing so the state is passed using React Router 'history' and 'location'.
-            Since I know Redux I wanted to try something new. There are functions like:
+              During this project I got to know more about routing so the state
+              is passed using React Router 'history' and 'location'. Since I
+              know Redux I wanted to try something new. There are functions
+              like:
               <p>• drawing random pairs of cards</p>
               <p>• shuffling cards</p>
               <p>• revealing only two cards at a time</p>
@@ -190,8 +154,10 @@ const Projects = () => {
             </h1>
             <div className="body-text">
               Forest Run is a fully functional React/Typescript application for
-              runners in London.  It is fully responsive. In this project I wanted to learn how to connect to the backend with GraphQL using Apollo Client.
-              Styles are based on Material-UI. State is managed by Redux and navigation by React-Router. <br />
+              runners in London. It is fully responsive. In this project I
+              wanted to learn how to connect to the backend with GraphQL using
+              Apollo Client. Styles are based on Material-UI. State is managed
+              by Redux and navigation by React-Router. <br />
               <br />
               <a
                 className="project-link"
@@ -223,8 +189,8 @@ const Projects = () => {
           <div className="text-content">
             <h1 className="title">React/Typescript</h1>
             <div className="body-text">
-              This project is similar to Memory Game. 
-              I was practicing Typescript.
+              This project is similar to Memory Game. I was practicing
+              Typescript.
               <p>• generate board with initial numbers</p>
               <p>• cleaning checked field</p>
               <p>• restarting game</p>
@@ -259,13 +225,13 @@ const Projects = () => {
           <div className="text-content">
             <h1 className="title">React/Typescript/Axios</h1>
             <div className="body-text">
-              Cointify is a React/Typescript app for
-              everyone who is interested in cryptocurrencies. It uses Axios to
-              send GET requests to Rest API provided by CoinGecko.com to display
-              current values of the coins. The design is based on Material-UI
-              css library. Navigation is managed by React-Router. Application is
-              not finished, the main purpose of the website is to send
-              notifications directly to a user’s mobile device. <br />
+              Cointify is a React/Typescript app for everyone who is interested
+              in cryptocurrencies. It uses Axios to send GET requests to Rest
+              API provided by CoinGecko.com to display current values of the
+              coins. The design is based on Material-UI css library. Navigation
+              is managed by React-Router. Application is not finished, the main
+              purpose of the website is to send notifications directly to a
+              user’s mobile device. <br />
               <a
                 className="project-link"
                 target="_blank"
